d5f8c103663901df528a2ef95b409251659b0442
Eric Mutuma <ericmutuma15@gmail.com>
2024-05-28 15:01:55 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+  
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if sorted(word.lower()) == sorted(self.word.lower()) and word.lower() != self.word.lower():
+                matches.append(word)
+        return matches
